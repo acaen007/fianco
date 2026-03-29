3ddc847eea7a765c55279f3c59037e00ade3241e
acaen7@gmail.com <acaen7@gmail.com>
2026-03-29 01:29:02 +0100

    removing old project

--- fianco_new/fianco_ai/target/x86_64-apple-darwin/debug/deps/fianco_ai 2.d
+++
@@ -1,5 +0,0 @@
-/Users/andycaen/Desktop/fianco_new/fianco_ai/target/x86_64-apple-darwin/debug/deps/libfianco_ai.dylib: src/lib.rs
-
-/Users/andycaen/Desktop/fianco_new/fianco_ai/target/x86_64-apple-darwin/debug/deps/fianco_ai.d: src/lib.rs
-
-src/lib.rs:
--- fianco_new/fianco_ai/target/x86_64-apple-darwin/debug/libfianco_ai 2.d
+++
@@ -1,1 +0,0 @@
-/Users/andycaen/Desktop/fianco_new/fianco_ai/target/x86_64-apple-darwin/debug/libfianco_ai.dylib: /Users/andycaen/Desktop/fianco_new/fianco_ai/src/lib.rs
